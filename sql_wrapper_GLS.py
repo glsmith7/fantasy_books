d408e1148a83d472e1a944aa25dfd5a181b5601b
glsmith7 <23667270+glsmith7@users.noreply.github.com>
2023-04-11 22:49:42 -0600

    Reading database works

--- sql_wrapper_GLS.py
+++ sql_wrapper_GLS.py
@@ -27,19 +27,40 @@ def retrieve_from_database(connection, query):
         log.logging.error ("There is no open connection to a database.")
         return sqlite3.DatabaseError ("There is no open connection to a database.")
     
-    if sqlite3.complete_statement(query) == False:
-          log.logging.error ("Incomplete or improper SQL query")
-          return sqlite3.DatabaseError ("Not a complete SQL statement?")
-          return 
+    # if sqlite3.complete_statement(query) == False:
+    #       log.logging.error ("Incomplete or improper SQL query")
+    #       return sqlite3.DatabaseError ("Not a complete SQL statement?")
+          
+    cursor = connection.cursor()
+    cursor.execute(query)
+    search_result = cursor.fetchall()
+    return search_result
+
+def print_search_results(search_result):
+    for row in search_result:
+        print (row)
+
+def gls1():
+
+    path = "testSQL.db3"
+    to_test = connect_to_database(path)
+    query = "SELECT * FROM TestTable01"
+    the_results = retrieve_from_database(to_test, query)
+    
+    print_search_results(the_results)
+    
+    for x in range(0,4): # there are 4 lines in test table
+        
+        print (x+1)
+        print(the_results[x][0]) # first column of each row is numbered 1-4; results start at zero, however, so plus 1.
+        print ("--")
 
 def main():
      log.setup_logging()
      log.start_logging()
      print ("Begin main.")
-     db = connect_to_database ("testSQL.db3")
-     print (retrieve_from_database(db,"B"))
+     gls1()
      print ("End of program")
-     db.close()
      log.end_logging()
  
 if __name__ == "__main__":
--- tests/test_database_access.py
+++ tests/test_database_access.py
@@ -22,13 +22,55 @@ def test_query_fails_for_lack_of_connection_object():
     assert type (sql_wrapper_GLS.retrieve_from_database("Not A Connection", "Not A Query")) is sqlite3.DatabaseError
 
 
-def test_query_fails_for_improper_SQL_query():
+
+
+
+def test_reading_database():
+    def test_column1(the_results,first_column,x):
+        assert (first_column) == the_results[x][0]
+
+    def test_column2(the_results,second_column,x):
+        assert (second_column) == the_results[x][1]
+
+    def test_column3(the_results,third_column,x):
+        assert (third_column) == the_results[x][2]
+
+    def test_column4(the_results,fourth_column,x):
+        assert (fourth_column) == the_results[x][3]
+    # connect
 
     path = "testSQL.db3"
-    to_test = sql_wrapper_GLS.connect_to_database(path) # need a database Object to pass to test
+    to_test = sql_wrapper_GLS.connect_to_database(path)
 
-    assert type (sql_wrapper_GLS.retrieve_from_database(to_test, "Not A Query")) is sqlite3.DatabaseError
+    # create SQL query
+    query = "SELECT * FROM TestTable01"
+    the_results = sql_wrapper_GLS.retrieve_from_database(to_test, query)
+    
+    sql_wrapper_GLS.print_search_results(the_results)
+
+    the_words = ("One","Two","Three","Four") # these are in the 3rd column of the test database.
+
+    for x in range(0,4): # there are 4 lines in test table
+        first_column = x+1 # Column 0 of each row is numbered 1-4, hence x+1.
+        second_column = str(first_column) + "d6+" + str(first_column)
+        third_column = the_words[x]
+        fourth_column = int(str(first_column) + str("123"))
+
+        test_column1(the_results,first_column,x)
+        test_column2(the_results,second_column,x)
+        test_column3(the_results,third_column,x)
+        test_column4(the_results,fourth_column,x)
+    
     to_test.close()
+
+
+# def test_query_fails_for_improper_SQL_query():
+
+#     path = "testSQL.db3"
+#     to_test = sql_wrapper_GLS.connect_to_database(path) # need a database Object to pass to test
+
+#     assert type (sql_wrapper_GLS.retrieve_from_database(to_test, "Not A Query")) is sqlite3.DatabaseError
+#     to_test.close()
     
 
 
