9c5a37758482cffe44a5ff6625ba0bb3467c5f99
glsmith7 <23667270+glsmith7@users.noreply.github.com>
2023-04-18 13:44:59 -0600

    Dice in tables parsing begins

--- sql_wrapper_GLS.py
+++ sql_wrapper_GLS.py
@@ -79,7 +79,7 @@ def get_2d_array(columns, results):
     return results
 
 def get_table_as_array(path, table_name, query = "Select * FROM(_replace_)", ): 
-    """ Combines various subroutines to get a 2D array in a single called step.
+    """ Combines various subroutines to get a 2D array with in a single called step.
     path = path to an SQLite database.
     table_name = table name
     query = optional SQL query. If omitted, returns entire table. If query includes "_replace_", the _replace_ string will be automatically replaced with the table name."""
@@ -99,17 +99,89 @@ def get_table_as_array(path, table_name, query = "Select * FROM(_replace_)", ):
     full_table_results = retrieve_from_database(dB, query)
 
     # create 2D array with column names as dictionary key words
-    final_array = get_2d_array (column_names, full_table_results)
+    table_as_array = get_2d_array (column_names, full_table_results)
     
-    return final_array
+    return table_as_array
+
+def convert_die_range_to_low_and_high(d):
+    """
+    Takes a 2-d table array as dictionary, and adds in the high and low dice values for each row.
+        
+        Converts 2- into 'DiceLow': -1000, 'DiceHigh':2 -- i.e., anything 2 or lower
+        Converts 12+ into 'DiceLow': 12, 'DiceHigh':1000 -- i.e., anything 12 or higher
+        Converts 3-7 into 'DiceLow': 3, 'DiceHigh':7
+        Converts 9 into 'DiceLow': 9, 'DiceHigh':9
+
+    NB: Direct copy/paste from ACKS PDF gives an en-dash, not a minus sign, for the "-" character, so be sure is in properly in the SQL database.
+    """
+
+# determine what kind of value in each row, returns true or false
+
+    def die_is_low_range(die_range): # eg, "2-", two or lower
+        pass
+
+    def die_is_high_range(die_range): # eg "12+", twelve or higher
+        pass
+    
+    def die_is_mid_range(die_range): # eg "7-9", ie, between 7 and 12.
+        pass
+
+    def die_is_single_digit(die_range): # e.g., 10, ie, 10 and only 10.
+        pass
+
+# Edit the rows by adding dice ranges to the dictionary and returning the edited row
+
+    def create_low_range_entries(row):
+        return row
+
+    def create_high_range_entries(row):
+        return row
+
+    def create_mid_range_entries(row):
+        return row
+
+    def create_single_digit_entries(row):
+        return row
+
+### Main routine
+
+    for row in d:
+        die_range = row["DieRange"]
+    
+    if die_is_low_range (die_range):
+        row = create_low_range_entries(row)
+    
+    elif die_is_high_range(die_range):
+        row = create_high_range_entries(row)
+
+    elif die_is_mid_range(die_range):
+        row = create_mid_range_entries(row)
+    
+    elif die_is_single_digit(die_range):
+        row = create_single_digit_entries(row)
+
+    else:
+        raise ValueError("The dice entry was not trapped anywhere. SQL table error?")
+       
+     
+        # if row ['DieRange'] == "2-":
+        #     print (row)
+        #     row['DieLow'] = -1000
+        #     row['DieLhigh'] = 2
+        #     print (row)
+        
+    return d
 
 def main():
      log.setup_logging()
      log.start_logging()
      print ("Begin main.")
 
-     path = "./SQLite_db/trialSQL.db3"
-     print (get_table_as_array(path,"TestTable01",query="SELECT * FROM '_replace_'"))
+     path = "./SQLite_db/ACKS_SQL_01.db3"
+     table_as_array = (get_table_as_array(path,"Surprise",query="SELECT * FROM '_replace_'"))
+     final_dictionary = (convert_die_range_to_low_and_high(table_as_array))
+     
+     print (final_dictionary)
 
      print ("End of program")
      log.end_logging()
