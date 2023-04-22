7b4fef07077744ad3554b03b06866ab5cbe439a0
glsmith7 <23667270+glsmith7@users.noreply.github.com>
2023-04-22 16:01:16 -0600

    Column names working now too

--- oop_tables_GLS.py
+++ oop_tables_GLS.py
@@ -31,9 +31,6 @@ class SQL_object_GLS:
         else:
              log.logging.info ("SLQ connection established: " + str(self.connection))
     
-    
-    
-  
 class RPG_table(SQL_object_GLS):
 
     def __init__ (self,table_name,query = s.SQL_QUERY_DEFAULT):
@@ -44,15 +41,17 @@ class RPG_table(SQL_object_GLS):
         self.query = self.query.replace("_replace_",self.table_name)
         self.retrieve_from_database()
         self.give_column_names()
+        self.get_2d_array()
 
-    
     
     def retrieve_from_database(self):
         
         if type(self.connection) is not sqlite3.Connection:
             log.logging.error ("There is no open connection to a database.")
-            return sqlite3.DatabaseError ("There is no open connection to a database.")
+            raise sqlite3.DatabaseError ("There is no open connection to a database.")
           
+        # self.query = "SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = 'TestTable01' ORDER BY ORDINAL_POSITION"
+        
         cursor = self.connection.cursor()
         cursor.execute(self.query)
         self.database_results = cursor.fetchall()
@@ -61,12 +60,36 @@ class RPG_table(SQL_object_GLS):
 
     
     def give_column_names(self):
+        query_column_names = "PRAGMA table_info({});".format(self.table_name) # returns column names from SQLite
+        cursor = self.connection.cursor()
+        cursor.execute(query_column_names)
+        column_results = cursor.fetchall()
+
         self.column_names = []
-        for row in self.database_results:
+        for row in column_results:
             self.column_names.append(row[1])
     
         log.logging.info ("Column names returned as: " + str(self.column_names) + "\n")
 
+    def get_2d_array(self):
+   
+        """ Returns a list with nested dictionaries. Each dictionary is a row of the table, with columns of the table as keywords
+    
+        j = iterates through each row
+        i = iterates through each column
+        """
+
+        for j in range (0,len(self.database_results)):
+            d = {} # Empty the dictionary before each row started
+
+            for i in range (0,len(self.column_names)):
+                d[self.column_names[i]] = self.database_results[j][i]
+            
+            self.database_results[j] = d # set the row of results to the dictionary value
+    
+        rows_and_columns_log_message = "There are {} columns and {} rows in the 2D array.".format(len(self.column_names),len(self.database_results))
+        log.logging.info ("2D array is returned as: " + str(self.database_results) + "\n")
+        log.logging.info (rows_and_columns_log_message + "\n")
     
   ################################
 
