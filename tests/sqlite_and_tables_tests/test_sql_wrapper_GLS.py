9cc1b89e425695a1d787e7b78062e8467aafe04c
glsmith7 <23667270+glsmith7@users.noreply.github.com>
2023-05-08 21:52:39 -0600

    Tests all work

--- tests/sqlite_and_tables_tests/test_sql_wrapper_GLS.py
+++ tests/sqlite_and_tables_tests/test_sql_wrapper_GLS.py
@@ -10,7 +10,7 @@ import tests.test_settings_GLS as s
 
 # proper test answers
 test_1_answer = sqlite3.Connection
-test_2_answer = ['Mercenary Type', 'Continent', 'Empire', 'Kingdom', 'Principality', 'Duchy', 'County', 'March', 'Barony', 'Restrictions', 'Race', 'Default Equipment']
+test_2_answer = ['Continent', 'Empire', 'Kingdom', 'Principality', 'Duchy', 'County', 'March', 'Barony', 'Restrictions', 'Race', 'Default Equipment']
 test_3_answer = [('Light Infantry', 340000, 28000, 6800, 1600, 360, 85, 17, 3, 'None', 'Human,Nomad,Elf, Kobold, Goblin, Orc, Gnoll, Hobgob, Lizardman, Bugbear, Ogre', '3 javelins, short sword, shield, leather armor')]
 
 # ## begin testing
@@ -31,7 +31,7 @@ def test_sql_wrapper_all_together_now():
     test_connection = tested_module.connect_to_database()
     assert  type(test_connection) == test_1_answer
     
-    test_4_answer = ['TestUniqueID', 'DiceFormula', 'DescriptionString', 'TestInteger01']
+    test_4_answer = ['DiceFormula', 'DescriptionString', 'TestInteger01']
     test_5_answer = [1, 2, 3, 4]
     test_6_answer = {1: {'DiceFormula': '1d6+1', 'DescriptionString': 'One', 'TestInteger01': 1123}, 2: {'DiceFormula': '2d6+2', 'DescriptionString': 'Two', 'TestInteger01': 2123}, 3: {'DiceFormula': '3d6+3', 'DescriptionString': 'Three', 'TestInteger01': 3123}, 4: {'DiceFormula': '4d6+4', 'DescriptionString': 'Four', 'TestInteger01': 4123}}
 
@@ -39,3 +39,9 @@ def test_sql_wrapper_all_together_now():
     column_names, row_names, final_dictionary_of_table = tested_module.get_table_as_dict (table_name="TestTable01", query = None, connection=test_connection, path = s.PATH_DEFAULT)
 
     assert column_names == test_4_answer
+    assert row_names == test_5_answer
+    assert final_dictionary_of_table == test_6_answer
+
+    # check the two mini routines that just give one of the three values. Same routine, so should always be the same as above.
+    assert tested_module.get_column_names(table_name="TestTable01", query = None, connection=test_connection, path = s.PATH_DEFAULT) == test_4_answer
+    assert tested_module.get_row_names(table_name="TestTable01", query = None, connection=test_connection, path = s.PATH_DEFAULT) == test_5_answer
