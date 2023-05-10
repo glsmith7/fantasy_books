224a6df18014e0ac23bff0692e90c4eb89e62adb
glsmith7 <23667270+glsmith7@users.noreply.github.com>
2023-05-09 19:18:00 -0600

    OOP Tables all work

--- oop_roll_on_tables_GLS.py
+++ oop_roll_on_tables_GLS.py
@@ -39,27 +39,28 @@ class SQL_object_GLS:
              logger.info ("SLQ connection established: " + str(self.connection))
     
 class RPG_table(SQL_object_GLS):
-    ''' SQL database object that represents an RPG table. '''
+    ''' Subclass of SQL database object that represents an RPG table. '''
+
     def __init__ (self,table_name,query = s.SQL_QUERY_DEFAULT,path=s.PATH_DEFAULT):
         super().__init__(path)
         self.pick_table (table_name,query)
         
 
     def pick_table (self,table_name,query= s.SQL_QUERY_DEFAULT): # can be used to pick a new table
-        '''Name of specific table in SQL database'''
+        ''' Combines all subroutines to prepare an RPG table for reference.'''
+
         self.table_name = table_name
         self.query = query
         self.query = self.query.replace("_replace_",self.table_name)
-        self.load()
-
-    def load(self): 
-        self.retrieve_from_database()
-        self.give_column_names()
-        self.get_2d_array()
+        self._retrieve_from_database()
+        self.get_column_names()
+        self._get_2d_array()
         if "DieRange" in self.column_names:
-            self.convert_die_range_to_low_and_high()
+            self._convert_die_range_to_low_and_high()
+        self._get_row_names()
+        self._finalize_table()
 
-    def retrieve_from_database(self):
+    def _retrieve_from_database(self):
         ''' Returns the SQL query results'''
         if type(self.connection) is not sqlite3.Connection:
             logger.error ("There is no open connection to a database.")
@@ -72,8 +73,9 @@ class RPG_table(SQL_object_GLS):
         self.database_results = cursor.fetchall()
 
         logger.info ("Search result of SQL database returned as: " + str(self.database_results) + "\n")
-    
-    def give_column_names(self):
+
+
+    def get_column_names(self):
         ''' Retrieves and stores the column names for the table '''
         query_column_names = "PRAGMA table_info({});".format(self.table_name) # returns column names from SQLite
         cursor = self.connection.cursor()
@@ -86,14 +88,21 @@ class RPG_table(SQL_object_GLS):
     
         logger.info ("Column names returned as: " + str(self.column_names) + "\n")
 
-    def get_2d_array(self):
+    def _get_row_names(self):
+        ''' Uses database_results to get the row names'''
+        self.row_names = []
+        for x in range (0,len(self.database_results)):
+            self.row_names.append(self.database_results[x][self.column_names[0]])
+
+
+    def _get_2d_array(self):
    
         """ Returns a list with nested dictionaries. Each dictionary is a row of the table, with columns of the table as keywords
     
         j = iterates through each row
         i = iterates through each column
         """
-
+        
         for j in range (0,len(self.database_results)):
             d = {} # Empty the dictionary before each row started
 
@@ -103,10 +112,22 @@ class RPG_table(SQL_object_GLS):
             self.database_results[j] = d # set the row of results to the dictionary value
     
         rows_and_columns_log_message = "There are {} columns and {} rows in the 2D array.".format(len(self.column_names),len(self.database_results))
-        logger.info ("2D array is returned as: " + str(self.database_results) + "\n")
+        logger.info ("Pre row-conversion 2D array is returned as: " + str(self.database_results) + "\n")
         logger.info (rows_and_columns_log_message + "\n")
+
+    def _finalize_table(self):
+
+        ''' Makes nested dictionary with row label as the key for the dictionary of each row, with each value keyed by column.'''
+
+        self.final_table = {}  
+        for y in range (0,len(self.row_names)):
+            self.database_results[y].pop(self.column_names[0]) # use COLUMN as the key to remove the duplicate value that is now the row_name. Will always be first (i.e., leftmost) column 
+            self.final_table.update ({self.row_names[y] : self.database_results[y]}) # dictionary key of row will serve as the the sub-dictionary for the rest of the row.
     
-    def convert_die_range_to_low_and_high(self):
+        # delete the first element of "column names" (i.e., 0,0 position of table, it has already been used for what we need above)
+        self.column_names.pop(0)
+    
+    def _convert_die_range_to_low_and_high(self):
         """
         Takes a 2-d table array as dictionary, and adds in the high and low dice values for each row.
             
@@ -214,19 +235,34 @@ class RPG_table(SQL_object_GLS):
                 raise ValueError("The dice entry was not trapped anywhere. SQL table error?")
 
     def roll (self,roll):
-
+        ''' Rolls on a table with a given number; if a DiceLow and DiceHigh field exists, then returns a value. Otherwise errors'''
         to_return={}
         for row in self.database_results:
-            if roll >= row['DieLow'] and roll <= row['DieHigh']:
-                to_return = row # ie, this row was rolled.
-                break
 
+            try:
+                if roll >= row['DieLow'] and roll <= row['DieHigh']:
+                    to_return = row # ie, this row was rolled.
+                    break
+            
+            except KeyError:
+                error_text = "Table {} in SQL database {} is not configured to have dice rolled on it.".format(self.table_name, self.path)
+                logging.exception(error_text)
+                raise KeyError (error_text)
+                
+            except Exception as err:
+                error_text = "Table {} in SQL database {} has encountered an error.".format(self.table_name, self.path)
+                logging.exception(error_text)
+                raise Exception(error_text) from err
+               
         if not to_return: # ie nothing has been put into to_return because no row matches
-
             error_text = "Nothing was found on table {} when rolling {} on it.".format(self.table_name, roll)
+            logging.error(error_text)
             raise KeyError (error_text)
         
-        return to_return        
+        return to_return
+    
+  
+
   ################################
 
 def main():
@@ -235,15 +271,17 @@ def main():
     print ("Running main of sql_table_object_GLS.")
    
     t = RPG_table(s.TABLE_NAME_DEFAULT)
+    
     print (t)
     print (t.path)
     print (t.connection)
     print (t.table_name)
     print (t.query)
     print (t.column_names)
-    print (t.database_results)
-    print (t.database_results[0]['Human'])
-
+    print (t.row_names)
+    print (t.final_table)
+    print (t.roll(10000))
+    
     t.connection.close()
     print ("End main of sql_table_object-GLS")
 
