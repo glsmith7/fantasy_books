3720fee8df72d5dddcec507f05819b5d78148d6f
glsmith7 <23667270+glsmith7@users.noreply.github.com>
2023-05-16 15:51:36 -0600

    Mercenary initial table display works with OOP style

--- mercenary_recruit.py
+++ mercenary_recruit.py
@@ -1,66 +1,36 @@
-import settings_GLS as s
-import sys
-from rich.text import Text
 from textual.app import App, ComposeResult
-from textual.containers import ScrollableContainer
-from textual.widgets import Button, Header, Footer, Static, Placeholder, DataTable
+from textual.widgets import DataTable, Header, Footer
+import sys
+import oop_roll_on_tables_GLS as rpg
 
-# logging boilerplate
-import logging
-import logging_tools_GLS
-logger = logging.getLogger(__name__)
-
-ROWS = [
-    ("lane", "swimmer", "country", "time"),
-    (2, "Michael Phelps", "United States", 51.14),
-    (5, "Chad le Clos", "South Africa", 51.14),
-    (6, "László Cseh", "Hungary", 51.14),
-    (3, "Li Zhuhao", "China", 51.26),
-    (8, "Mehdy Metella", "France", 51.58),
-    (7, "Tom Shields", "United States", 51.73),
-    (1, "Aleksandr Sadovnikov", "Russia", 51.84),
-    (10, "Darren Burns", "Scotland", 51.84),
-]
-
-
-class Test(Static):
-    
-
-    def compose(self) -> ComposeResult:
-        yield DataTable()
+class MercTable(DataTable):
 
     def on_mount(self) -> None:
-       
-        table = self.query_one(DataTable)
-        table.add_columns(*ROWS[0])
-        for row in ROWS[1:]:
-            # Adding styled and justified `Text` objects instead of plain strings.
-            # styled_row = [
-            #     Text(str(cell), style="italic #03AC13", justify="right") for cell in row
-            # ]
-            table.add_row(*row)
-
-class MercenaryApp(App):
-    BINDINGS = [
-        ("d", "toggle_dark", "Toggle dark mode"),
-        ("q", "quit_app","Quit app")
-        ]
-
-    def compose(self):
-        yield Header()
-        yield Test()
-        yield Footer()
+        table = rpg.RPG_table(self.id)
+        self.add_columns(*table.column_names_full)
+        self.add_rows(table.table_list_rows[0:])
     
-    def on_mount(self):
-        pass
+class TableApp(App):
+    # CSS_PATH = 'mercenary_recruit.css'
+    BINDINGS = [
+        ("q,Q", "quit", "Quit Program"),
+    ]
+    
+    def compose(self) -> ComposeResult:
+        yield MercTable(id="SlaveTroopCost")
+        yield Header()
+        yield Footer()
 
-    def action_toggle_dark(self) -> None:
-        """An action to toggle dark mode."""
-        self.dark = not self.dark
+    def quit(self):
+        sys.quit(0)
 
-    def action_quit_app(self):
-        sys.exit(0)
-
-if __name__ == "__main__":
-    app = MercenaryApp()
+def main():
+    app = TableApp()
     app.run()
+if __name__ == "__main__":
+    main()
+
+
+
+
+    
