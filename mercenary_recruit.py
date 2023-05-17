9daee78d4317d31129a404883070d4f302326019
glsmith7 <23667270+glsmith7@users.noreply.github.com>
2023-05-16 23:50:35 -0600

    Much mucking about; autogenerates race list from SQL

--- mercenary_recruit.py
+++ mercenary_recruit.py
@@ -1,14 +1,20 @@
 from textual.app import App, ComposeResult
-from textual.containers import Grid, Horizontal
-from textual.screen import Screen
+from textual.containers import Grid, Horizontal, Vertical
+from textual.screen import ModalScreen, Screen
 from textual.widgets import Button, DataTable, Footer, Header, Label, RadioButton, RadioSet
+from textual import errors
 import sys
 import oop_roll_on_tables_GLS as rpg
 
-class QuitScreen(Screen):
+# logging boilerplate
+import settings_GLS as s
+import logging
+import logging_tools_GLS
+logger = logging.getLogger(__name__)
+
+class QuitScreen(ModalScreen):
     """Screen with a dialog to quit."""
-    CSS_PATH = 'quit_screen.css'
-    
+   
     def compose(self) -> ComposeResult:
         yield Grid(
             Label("Are you sure you want to quit?", id="question"),
@@ -26,45 +32,45 @@ class QuitScreen(Screen):
 class RaceSelectScreen(Screen):
     '''Dialogue box to pick race of troops'''
     CSS_PATH = 'radio_set.css'
-    BINDINGS = [
-        ("r",'race_pick', 'Race pick'),
-    ]
+        
     def compose(self) -> ComposeResult:
+        the_default = 'Human' # change to whatever is the current choice once figure out how to do that.
+        the_table = rpg.RPG_table('RacesAvailable')
+        list_of_race_options = the_table.database_results
+        
         with Horizontal():
             with RadioSet(id="race_select"):
-                yield RadioButton("Human", value = True)
-                yield RadioButton("Dwarf")
-                yield RadioButton("Elf")
-                yield RadioButton("Gnoll")
-                yield RadioButton("Orc")
-                yield RadioButton("Goblin")
-                yield RadioButton("Kobold")
-                yield RadioButton("Quit")
+                for row in list_of_race_options:
+                    if row['Race'] != the_default:
+                        yield RadioButton(row['Race'])
+                    else:    
+                        yield RadioButton(row['Race'], value=True)
+        
 
     def on_mount (self) -> None:
-        self.query_one('#race_select')
-    
-    def action_race_pick(self) -> None:
-        self.app.pop_screen()
+        self.query_one('#race_select').focus('Human')
+        pass
 
 class MercTable(DataTable):
 
     def on_mount(self) -> None:
         
-        table = rpg.RPG_table(self.id, query = "SELECT mercenary_type, equipment,class_1 FROM MercenaryTableCities WHERE races_avail LIKE '%man%'")
+        table = rpg.RPG_table(self.id,query = "SELECT mercenary_type, equipment,class_1 FROM MercenaryTableCities WHERE races_avail LIKE '%man%'")
         self.add_columns(*table.column_names_full)
         self.add_rows(table.table_list_rows[0:])
     
 class TableApp(App):
+
     CSS_PATH = 'mercenary_recruit.css'
     BINDINGS = [
-        ("q,Q", "quit", "Quit Program"),
-        ("r",'race_pick', 'Race pick'),
-        ("c",'city_class_pick', 'Class market pick'),
+        ('q', 'quit', 'Quit Program'),
+        ('r','race_pick', 'Race pick'),
+        ('c','city_class_pick', 'Class market pick'),
+        ('escape','escape','Escape back')
     ]
 
     def compose(self) -> ComposeResult:
-        yield MercTable(id="MercenaryTableCities")
+        yield MercTable(id='MercenaryTableCities')
         yield Header()
         yield Footer()
 
@@ -74,10 +80,14 @@ class TableApp(App):
     def action_race_pick(self):
         self.push_screen(RaceSelectScreen())
 
-    
+    def action_escape(self):
+        try:
+            self.pop_screen()
+        except:
+            pass # errors with ScreenStackError, but doesn't identify this, so just trapping all error here and passing it. Not great, but ....
 
 def main():
     app = TableApp()
     app.run()
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
--- settings_GLS.py
+++ settings_GLS.py
@@ -1,7 +1,7 @@
 # settings file
 
 PATH_DEFAULT = "./sqlite_db/ACKS_SQL_01.db3"
-SQL_QUERY_DEFAULT = "SELECT mercenary_type, class_1 FROM '_replace_'"
+SQL_QUERY_DEFAULT = "SELECT * FROM '_replace_'"
 TABLE_NAME_DEFAULT = "MercenaryTableCities"
 
 # PATH_DEFAULT = "./tests/testSQL.db3"
