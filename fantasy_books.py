4ec1ceb5be1f3d964b595ef1041cbfd66ff45d09
glsmith7 <23667270+glsmith7@users.noreply.github.com>
2023-05-22 20:36:02 -0600

    moved stuff into FantasyBook methods

--- fantasy_books.py
+++ fantasy_books.py
@@ -1,12 +1,12 @@
 import oop_roll_on_tables_GLS as r
-import 
 import random as random
- 
+
 global the_list_of_tables
 global list_of_names_tables_male, list_of_names_tables_female
 global name_tables_male, name_tables_len_male, name_table_amalgamated_male 
 global name_tables_female, name_tables_len_female, name_table_amalgamated_female
 global author_title_tables
+global complexity_table_list
 
 the_list_of_tables = [ # SQL table name first, then self.variable for the book object. Make sure has a aaDiceTypeToRoll table entry for each table.
         ("BookScope","scope"),
@@ -43,8 +43,7 @@ list_of_surnames_tables = [
 		"_names_norse_surnames_male",
         "_names_saints_surnames", 
         ]
-
-
+complexity_table_list = ["BookComplexityForScope1","BookComplexityForScope2","BookComplexityForScope3","BookComplexityForScope4"]
 
 name_tables_male, name_tables_len_male = {}, {}
 name_tables_female, name_tables_len_female = {}, {}
@@ -78,40 +77,7 @@ def create_fantasy_book(type=None, **kwargs):
     else:
         return FantasyBook(**kwargs)
     
-def generate_book_details_from_tables(b,table_for_value):
-    ''' Checks table aaDiceTypeToRoll to see what dice to roll, and then rolls and checks the result on the given table.
-    All tables that use this should have:
-    1) An entry in SQL table aaDiceTypeToRoll with the same table name that matches table to roll on.
-    2) The SQL table to be rolled on should have only two columns: one called 'DieRange' and the other 'Result'. '''
 
-    # what dice to roll on the table   
-    dice = r.what_dice_to_roll(table_for_value) # returns a list
-    if dice == []:
-        raise ValueError("Empty dice list returned -- does SQL table 'aaDiceTypeToRoll' have an entry for this table?")
-    else:
-        dice_string = dice[0] # dice[0] is the only thing returned, and this makes it a string rather than list as dice is.
-        roll_result = r.d20.roll(dice_string) 
-
-    # actually do the roll now that we know what dice we're rolling
-    t = r.RPG_table(table_for_value)
-    rolled_row = t.roll(roll_result.total) # .total sends only the integer total, nil else.
-    return rolled_row['Result'] 
-
-def randomize_book_details(the_book):
-    ''' Loops through all variables and assigns randomly based on random SQL table rolls.'''
-    
-    # generate from the global
-
-    for i,j in the_list_of_tables:
-        setattr(the_book,j,generate_book_details_from_tables(the_book,i)) # sets variable J of object the_book to the rolled results on table i for each element.
-    
-    # some with more specific rolling requirements.
-    randomize_book_complexity(the_book)
-
-
-def randomize_book_complexity(the_book):
-    complexity_table_list = ["BookComplexityForScope1","BookComplexityForScope2","BookComplexityForScope3","BookComplexityForScope4"]
-    setattr(the_book,"complexity",generate_book_details_from_tables(the_book,complexity_table_list[the_book.scope-1])) # Minus 1 since index of list starts at zero.
 
 class FantasyBook():
     ''' Fantasy book object.'''
@@ -133,8 +99,8 @@ class FantasyBook():
         number_extant_copies = 0,
         number_extant_available_to_place = 0,
         scope = 0,
-        complexity = 5,
-        age = 2,
+        complexity = 0,
+        age = 0,
         number_pages = 0,
         reading_time = 0,
         reference_time = 0,
@@ -146,6 +112,8 @@ class FantasyBook():
         weight = 0,
         number_volumes = 0,
         ):
+        
+        self.randomize_book_details() # scope, 
 
         self.topic = topic
         self.title = title
@@ -154,8 +122,7 @@ class FantasyBook():
         self.translator = translator
         self.original_language = original_language
         self.translated_language = translated_language
-        self.scope = scope
-        self.complexity = complexity
+        self.set_complexity(complexity)
         self.age = age
         self.format = format
         self.materials = materials
@@ -173,10 +140,45 @@ class FantasyBook():
         self.weight = weight
         self.number_volumes = number_volumes
 
+    def book_details_result_from_tables(self,table_for_value):
+        ''' Checks table aaDiceTypeToRoll to see what dice to roll, and then rolls and checks the result on the given table.
+        All tables that use this should have:
+        1) An entry in SQL table aaDiceTypeToRoll with the same table name that matches table to roll on.
+        2) The SQL table to be rolled on should have only two columns: one called 'DieRange' and the other 'Result'. '''
+
+        # what dice to roll on the table   
+        dice = r.what_dice_to_roll(table_for_value) # returns a list
+        if dice == []:
+            raise ValueError("Empty dice list returned -- does SQL table 'aaDiceTypeToRoll' have an entry for this table?")
+        else:
+            dice_string = dice[0] # dice[0] is the only thing returned, and this makes it a string rather than list as dice is.
+            roll_result = r.d20.roll(dice_string) 
+
+        # actually do the roll now that we know what dice we're rolling
+        t = r.RPG_table(table_for_value)
+        rolled_row = t.roll(roll_result.total) # .total sends only the integer total, nil else.
+        return rolled_row['Result'] 
+    
     def add(self,library):
         ''' Add this book to a given library'''
         self.libraries_it_is_in.append(library)
+
+
+    def randomize_book_details(self):
+        ''' Loops through all variables and assigns randomly based on random SQL table rolls.'''
     
+        for i,j in the_list_of_tables:
+            setattr(self,j,self.book_details_result_from_tables(i)) # sets variable J of object the_book to the rolled results on table i for each element.
+    
+    def set_complexity(self,complexity):
+        if not complexity:
+            complexity_from_table = self.book_details_result_from_tables(complexity_table_list[self.scope-1]) # Minus 1 since index of list starts at zero.
+
+            setattr(self,"complexity",complexity_from_table[0]) # index 0 converts to string eg 1, instead of list ['1']
+            print ("pause")
+        else:
+            self.complexity = complexity
+
     def remove (self,library):
         ''' Remove this book from a given library'''
         try:
@@ -250,19 +252,14 @@ class AuthoritativeBook(FantasyBook):
         self.authoritative_field = authoritative_field
         self.authority_rank = authority_rank
 
-
 ############################
 # main()
-
-# SELECT count(0) from aaDiceTypeToRoll is SQL for number of lines in a table.
 init_program_load_tables()
-
 number_to_run = 100
 
 for z in range(0,number_to_run):
 
     a = create_fantasy_book()
-    randomize_book_details(a)
     print ("Scope:" + str(a.scope))
     print ("Lang:" + str(a.original_language))
     print ("Complex:" + str(a.complexity))
